9ee560ff5d0eb98f0af99f5a136867d99353bdf9
Rok Oblak <oblakr24@gmail.com>
2023-05-17 20:23:29 +0200

    Minor cleanup

--- app/src/main/java/com/rokoblak/gittrendingcompose/data/db/ReposDatabase.kt
+++ app/src/main/java/com/rokoblak/gittrendingcompose/data/db/ReposDatabase.kt
@@ -7,7 +7,7 @@ import com.rokoblak.gittrendingcompose.data.db.model.GitRepoEntity
 
 const val REPOS_DB_VERSION = 1
 
-@Database(entities = [GitRepoEntity::class], version = REPOS_DB_VERSION)
+@Database(entities = [GitRepoEntity::class], version = REPOS_DB_VERSION, exportSchema = false)
 abstract class ReposDatabase : RoomDatabase() {
 
     abstract fun reposDao(): ReposDao
